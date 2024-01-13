c66a9bfe8729b7600b693abeafc8e9182eadcd86
isaac20212021 <marciojesus186@gmail.com>
2024-01-13 13:40:09 -0300

    fix: ajustes layout geral

--- src/containers/depoimentos/index.tsx
+++ src/containers/depoimentos/index.tsx
@@ -7,12 +7,13 @@ export function Depoimentos() {
   return (
     <div>
       <div className={styles.container}>
-        <div className={styles.infoStyle}>
-          <div className={styles.containerTitle}>
+        <div className={styles.infoStyle}></div>
+        <div className={styles.containerTitle}>
+          <div className={styles.infoStyle}>
             <h2>AVALIAÇÕES E DEPOIMENTOS DE NOSSOS CLIENTES</h2>
-            <hr style={{ width: "50%", color: "#FFF", borderWidth: 1 }} />
-            <CarouselList />
           </div>
+          <hr style={{ width: "50%", color: "#FFF", borderWidth: 1 }} />
+          <CarouselList />
         </div>
       </div>
     </div>
