e7fd11154e39575bd4df47adc36014a402ba49e1
isaac20212021 <marciojesus186@gmail.com>
2024-01-22 23:16:44 -0300

    fix: adiciona ajustes subheader

--- src/components/subHeader/index.tsx
+++ src/components/subHeader/index.tsx
@@ -28,68 +28,68 @@ export function SubHeader() {
         theme === 2 ? styles.headerContainerB : styles.headerContainer
       }`}
     >
-      <div className={styles.containerButton}>
-        <img src="/images/solar-meta.png" alt="Satisfação" width={"11%"} />
-        <img
-          src="/images/banho.svg"
-          style={{
-            marginTop: "2.4%",
-            marginLeft: "12.5%",
-            width: 22,
-            height: 22,
-            position: "absolute",
-            zIndex: 3,
-          }}
-        />
-        <Button
-          type={5}
-          width={240}
-          heigth={44}
-          text={"Aquecedor para Banho"}
-          onClick={() => alert("clicou")}
-          marginLeft={"12%"}
-          paddingLeft={"1.4%"}
-        />
-        <img
-          src="/images/piscina.svg"
-          style={{
-            marginTop: "2.5%",
-            marginLeft: 8,
-            width: 23,
-            height: 21,
-            position: "absolute",
-            zIndex: 3,
-          }}
-        />
-        <Button
-          type={5}
-          width={240}
-          heigth={44}
-          text={"Aquecedor para Piscina"}
-          onClick={() => alert("clicou")}
-          paddingLeft={"1.4%"}
-          //marginLeft={"90"}
-        />
-        <img
-          src="/images/predio.svg"
-          style={{
-            marginTop: "2.5%",
-            marginLeft: 9,
-            width: 24,
-            height: 19,
-            position: "absolute",
-            zIndex: 3,
-          }}
-        />
-        <Button
-          type={5}
-          width={240}
-          heigth={44}
-          text={"Prédios e Condomínios"}
-          onClick={() => alert("clicou")}
-          paddingLeft={"1.4%"}
-          // marginLeft={"90"}
-        />
+      <div className={styles.headerContainer}>
+        <div className={styles.containerButton}>
+          <img src="/images/solar-meta.png" alt="Satisfação" width={"11%"} />
+          <img
+            src="/images/banho.svg"
+            style={{
+              marginTop: "2.4%",
+              marginLeft: "12.5%",
+              width: 22,
+              height: 22,
+              position: "absolute",
+              zIndex: 3,
+            }}
+          />
+          <Button
+            type={5}
+            width={240}
+            heigth={44}
+            text={"Aquecedor para Banho"}
+            onClick={() => alert("clicou")}
+            marginLeft={"12%"}
+            paddingLeft={"1.4%"}
+          />
+          <img
+            src="/images/piscina.svg"
+            style={{
+              marginTop: "2.5%",
+              marginLeft: 8,
+              width: 23,
+              height: 21,
+              position: "absolute",
+              zIndex: 3,
+            }}
+          />
+          <Button
+            type={5}
+            width={240}
+            heigth={44}
+            text={"Aquecedor para Piscina"}
+            onClick={() => alert("clicou")}
+            paddingLeft={"1.4%"}
+          />
+          <img
+            src="/images/predio.svg"
+            style={{
+              marginTop: "2.5%",
+              marginLeft: 9,
+              width: 24,
+              height: 19,
+              position: "absolute",
+              zIndex: 3,
+            }}
+          />
+          <Button
+            type={5}
+            width={240}
+            heigth={44}
+            text={"Prédios e Condomínios"}
+            onClick={() => alert("clicou")}
+            paddingLeft={"1.4%"}
+          />
+        </div>
       </div>
     </header>
   );
